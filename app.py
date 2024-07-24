152cc6aacc612d3218c62c5a44b9af3beba5d758
Dalwinder576 <da4368576@alphacollege.me>
2024-07-24 18:58:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class MyApp:
         self.documents = []
         self.embeddings = None
         self.index = None
-        self.load_pdf("THEDIA1.pdf")
+        self.load_pdf("Nutrition.pdf")
         self.build_vector_db()
 
     def load_pdf(self, file_path: str) -> None:
@@ -53,7 +53,7 @@ def respond(
     temperature: float,
     top_p: float,
 ):
-    system_message = "You are a knowledgeable DBT coach. You always talk about one options at at a time. you add greetings and you ask questions like real counsellor. Remember you are helpful and a good listener. You are concise and never ask multiple questions, or give long response. You response like a human counsellor accurately and correctly. consider the users as your client. and practice verbal cues only where needed. Remember you must be respectful and consider that the user may not be in a situation to deal with a wordy chatbot.  You Use DBT book to guide users through DBT exercises and provide helpful information. When needed only then you ask one follow up question at a time to guide the user to ask appropiate question. You avoid giving suggestion if any dangerous act is mentioned by the user and refer to call someone or emergency."
+    system_message = "The main focus is on proving through the examination of various literature and the effects of specific nutrients that nutrition has a great impact on mental and physical well-being."
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -85,22 +85,20 @@ demo = gr.Blocks()
 
 with demo:
     gr.Markdown(
-        "‼️Disclaimer: This chatbot is based on a DBT exercise book that is publicly available. and just to test RAG implementation.‼️"
+        
     )
     
     chatbot = gr.ChatInterface(
         respond,
         examples=[
-            ["I feel overwhelmed with work."],
-            ["Can you guide me through a quick meditation?"],
-            ["How do I stop worrying about things I can't control?"],
-            ["What are some DBT skills for managing anxiety?"],
-            ["Can you explain mindfulness in DBT?"],
-            ["I am interested in DBT excercises"],
-            ["I feel restless. Please help me."],
-            ["I have destructive thoughts coming to my mind repetatively."]
+            ["What are the effects of nutrition upon the physical development and health of an individual?"],
+            ["How nutrition is a key component for mental development?"],
+            ["Why Nutrition is important?"],
+            ["How Vitamin A is a very important nutrient?"],
+            ["How Vitamin C helps boost the body’s ability?"],
+            ["Carnitine is a nutrient similar to some vitamins. Justify it."],
         ],
-        title='Dialectical Behaviour Therapy Assistant👩‍⚕️🧘‍♀️'
+        title='Importance of Nutrition'
     )
 
 if __name__ == "__main__":
